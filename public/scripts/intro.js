176779393cf50216c6f25f05568c141c8df7e8d7
Luccas Yuri dos Santos <luccas.yuri@hotmail.com>
2020-06-06 15:53:37 -0300

    Project Ecoleta

---
+++ public/scripts/intro.js
@@ -0,0 +1,58 @@
+//let myvar ="hello world"
+//const myconst = "he"
+
+//document.write(myconst + myvar)
+
+//string
+
+//const s1 = "isso é uma string"
+//const s2 = 'isso também é um string'
+
+//number
+//const n1 = "1"
+//const n2 = 12
+//document.write(n1 + n2)
+
+//boolean =  true or false
+
+//const bTrue =true
+//const bFalse = false
+//document.write(bTrue)
+
+//objetopossuem propriedades e funcionalidades
+// const pessoa = {
+//  altura: "1,80m",
+//  idade:24,
+//  solteiro:true,
+//   correr(){
+//     document.write("Run Forest")
+//     return "run Forest"
+//   }
+
+// }
+
+// document.write(pessoa.correr())/
+
+
+//array - vetores
+// const colecaoDeBolinha =["blue", "green",1,{name:"My name"}]
+
+// document.write(colecaoDeBolinha[3].name)
+
+//funções - Funcionalidades - atalhos - reuso de codigos
+
+//função registrada
+// 
+
+// const notaFinal = 7
+
+// if(notaFinal < 5){
+//   document.write("Reprovado")
+// }else{
+//   document.write("Aprovado")
+// }
+
+// for(i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+
